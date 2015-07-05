d0ad4ce76b69212021bd77e5f0dd097954502012
unknown <felipe1740@gmail.com>
2015-07-05 13:25:48 -0300

    Renomeando MainForm

--- N.Rainhas/MainForm.cs
+++ N.Rainhas/MainForm.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace N.Rainhas
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {   
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
             selecaoBox.SelectedIndex = 0;
